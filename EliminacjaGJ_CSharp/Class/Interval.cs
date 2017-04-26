9c41e28c34ef78d5e6bde6a13723e32ab08bcb5e
jakub-tomczak <jakub.tomczak@live.com>
2017-04-26 12:09:33 +0200

    Add C# project, add mpfr dynamic dependency, add Interval generic class, add Matrix generic class.

---
+++ EliminacjaGJ_CSharp/Class/Interval.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliminacjaGJ_CSharp.Class
+{
+    class Interval<T>
+    {
+        public Interval(T a, T b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        T a;
+        T b;
+
+
+        public T A
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                a = value;
+            }
+        }
+
+        public T B
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                b = value;
+            }
+        }
+
+        public static Interval<T> operator+(Interval<T> a, Interval<T> b)
+        {
+            return new Interval<T>((dynamic)a.a + b.a, (dynamic)a.b + b.b);
+        }
+
+    }
+}
---
+++ EliminacjaGJ_CSharp/Class/Matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliminacjaGJ_CSharp.Class
+{
+    class Matrix<T>
+    {
+        private T [,] matrix;
+        public Matrix(int rows, int columns)
+        {
+            matrix = new T[rows, columns];
+        }
+
+        public static Matrix<T> GenerateRandomMatrix(int rows, int columns)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

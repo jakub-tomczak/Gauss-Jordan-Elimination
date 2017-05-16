c5db0b6cd4a8815b5ce5fab1576dc2bb65a2a4ca
jakub-tomczak <jakub.tomczak@live.com>
2017-05-16 14:32:48 +0200

    Add Gauss elimination.

--- EliminacjaGJ_CSharp/MainWindow.xaml.cs
+++ EliminacjaGJ_CSharp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EliminacjaGJ_CSharp
 {
@@ -23,6 +24,50 @@ namespace EliminacjaGJ_CSharp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        const int n = 100;
+        Matrix<double> matrix = Matrix<double>.Build.Random(n, n);
+        Vector<double> vectorB = Vector<double>.Build.Random(n);
+
+        private void DisplayMatrix(int dim)
+        {
+            //for(int i=1;i<dim;i++)
+            //{
+            //    for(int j=1;j<dim;j++)
+            //    {
+            //        Console.Write(string.Format("{1}\t", matrix[i, j]));
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            Console.WriteLine(matrix.ToString(n, n));
+        }
+
+
+        public void Gauss()
+        {
+            
+            for (int gw = 0; gw < n - 1; gw++)
+            {
+                for (int w = gw + 1; w < n; w++)
+                {
+                    double m = matrix[w, gw] / matrix[gw,gw];
+                    for(int k = gw; k<n;k++)
+                    {
+                        matrix[w, k] = matrix[w, k] - m * matrix[gw, k];
+                    }
+                    vectorB[w] = vectorB[w] - m * vectorB[gw];
+                }
+            }
+
+
+        }
+
+        private void Gauss(object sender, RoutedEventArgs e)
+        {
+            Gauss();
+            DisplayMatrix(n);
         }
     }
 }
